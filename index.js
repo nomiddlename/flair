9962d3c4dbf3ea2ad02fac1518c08cbfd44e6f34
Gareth Jones <gareth.jones@sensis.com.au>
2013-05-09 09:05:55 +1000

    starting to get routes documented

--- index.js
+++ index.js
@@ -1,22 +1,62 @@
 var express = require('express');
 
 exports.swagger = swagger;
+exports.describe = describe;
 
-function api() {
-  return {
-    apiVersion: "1.0",
+function api(options, routes) {
+  var doc = {
+    apiVersion: ((options && options.version) || "1.0"),
     swaggerVersion: "1.1",
-    basePath: "/",
+    basePath: ((options && options.basePath) || "/"),
     apis: []
-  };
+  },
+  apiPaths = [];
+
+  routes.forEach(function(route) {
+    var base = route.path.split('/')[0];
+    if (apiPaths.indexOf(base) < 0) {
+      apiPaths.push(base);
+    }
+  });
+  
+  return doc; 
 }
 
-function swagger() {
-  var app = express();
+function documentedRoutes(app) {
+  var routes = [];
+  
+  Object.keys(app.routes).forEach(function(method) {
+    var interestingRoutes = app.routes[method].filter(function(route) {
+      return route.callbacks.filter(function(cb) { return cb.shortDescription; });
+    });
+    routes.push(interestingRoutes);
+  });
+
+  return routes;
+}
+
+function swagger(appToDocument, options) {
+  if (!(appToDocument && appToDocument.routes)) {
+    throw new Error("Either you didn't pass in an express app, or that app had no routes defined");
+  }
+
+  var app = express()
+  , apiDoc = api(options, documentedRoutes(appToDocument));
+
+  console.log(appToDocument.routes);
+ 
   app.get('/', function(req, res) {
-    res.json(api());
+    res.json(apiDoc);
   });
   
   return app;
+}
+
+function describe(shortDescription, longDescription) {
+  var middleware = function(req, res, next) { next(); };
+  middleware.shortDescription = shortDescription;
+  middleware.longDescription = longDescription;
+
+  return middleware;
 }
 
--- test/flair-test.js
+++ test/flair-test.js
@@ -1,10 +1,16 @@
 var should = require('should')
 , supertest = require('supertest')
-, flair = require('../index');
+, express = require('express')
+, flair = require('../index')
+;
 
 describe('flair', function() {
   describe('#swagger', function() {
-    var app = flair.swagger();
+    var app = flair.swagger(express());
+
+    it('should expect an express app to document', function() {
+      (function() { flair.swagger() }).should.throw();
+    });
 
     it('should return an express app', function() {
       app.should.be.a('function');
@@ -16,20 +22,78 @@ describe('flair', function() {
 
     it('should respond with a swagger-compliant json doc', function(done) {
       supertest(app)
-      .get('/')
-      .set('Content-Type', 'application/json')
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200)
-      .end(function(err, res) {
-        if (err) {
-          done(err);
-        }
-        res.body.should.have.property('apiVersion').equal('1.0');
-        res.body.should.have.property('swaggerVersion').equal('1.1');
-        res.body.should.have.property('basePath').equal('/');
-        res.body.should.have.property('apis');
-        done(err);
+        .get('/')
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            done(err);
+          }
+          res.body.should.have.property('apiVersion').equal('1.0');
+          res.body.should.have.property('swaggerVersion').equal('1.1');
+          res.body.should.have.property('basePath').equal('/');
+          res.body.should.have.property('apis');
+          done();
+        });
+    });
+
+    describe('when provided with an api version', function() {
+      var app = flair.swagger(express(), { version: "0.5" });
+      
+      it('should respond with that version', function(done) {
+        supertest(app)
+          .get('/')
+          .set('Content-Type', 'application/json')
+          .expect(200)
+          .end(function(err, res) {
+            res.body.apiVersion.should.equal('0.5');
+            done(err);
+          });
       });
     });
+
+    describe('when provided with a base path', function() {
+      var app = flair.swagger(express(), { basePath: "/pants" });
+      
+      it('should respond with that version and basePath', function(done) {
+        supertest(app)
+          .get('/')
+          .set('Content-Type', 'application/json')
+          .expect(200)
+          .end(function(err, res) {
+            res.body.basePath.should.equal("/pants");
+            done(err);
+          });
+      });
+    });
+
+    describe('when a route has been described', function() {
+      var app = express(), flairedApp;
+      app.get(
+        '/pants', 
+        flair.describe("short pants", "longer pants"),
+        function (req, res) {
+          res.send("I'm still here.");
+        }
+      );
+      
+      flairedApp = flair.swagger(app);
+
+      it('should include the top-level resource path in the apis array', function(done) {
+        supertest(flairedApp)
+          .get('/')
+          .set('Content-Type', 'application/json')
+          .expect(200)
+          .end(function(err, res) {
+            res.body.apis.should.have.length(1);
+            res.body.apis[0].path.should.equal('pants');
+            res.body.apis[0].description.should.equal('none');
+            done(err);
+          });
+      });
+
+    });
+
   });
 });
